b026fafd252bbf16c428ade9495c345001faf970
Shuva Dev <s.r.shuvadeb@gmail.com>
2025-03-10 11:27:52 +0600

    AdminLTE configured

---
+++ PSZ.LMS/src/PSZ.LMS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSZ.LMS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
