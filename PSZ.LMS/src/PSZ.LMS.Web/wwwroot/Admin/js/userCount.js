90fadd2e234d1eb45e44e86acd2daab1aac01f9e
Shuva Dev <s.r.shuvadeb@gmail.com>
2025-03-29 19:38:10 +0600

    Removed AdminLTE and NiceAdmin Integrated

---
+++ PSZ.LMS/src/PSZ.LMS.Web/wwwroot/Admin/js/userCount.js
@@ -0,0 +1,28 @@
+﻿// Create Connection
+var connnectionUserCount = new signalR.HubConnectionBuilder().withUrl("hubs/UserCount").build();
+
+// Connect to methods that hub invokes aka receive notifications from hub
+connnectionUserCount.on("updateTotalViews", (value) => {
+    let newCount = document.getElementById("totalViewsCounter");
+    newCount.innerText = value.toString();
+});
+
+connnectionUserCount.on("updateTotalActiveUsers", (value) => {
+    let newCount = document.getElementById("totalActiveUsersCounter");
+    newCount.innerText = value.toString();
+});
+
+
+// Invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connnectionUserCount.invoke("NewWindowLoaded").then((value) => console.log(value));
+}
+
+// Start connection
+function fulfilled() {
+    console.log("Connection to User Hub Successful");
+    newWindowLoadedOnClient();
+}
+function rejected() {}
+
+connnectionUserCount.start().then(fulfilled, rejected);
